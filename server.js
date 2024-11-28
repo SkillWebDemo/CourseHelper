7dff1111e819261c4665137c1c03daa788f50f24
acchas <178907090+acchas@users.noreply.github.com>
2024-11-28 10:14:54 +0100

    Test example

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const path = require("path");
 const app = express();
 const listenPost = 3000;
 
+
 app.use(express.static(path.join(__dirname, "public")));
+
+//This forwards the test endpoint to the test-router module
+app.use("/test", require("./routes/test"));
 
 //Listen to port 3000
 app.listen(listenPost, () => {
